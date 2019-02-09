d30955e2fe31c055d6e21608e10678a4d6848bcf
Ethan Rylko <ethan.rylko@gmail.com>
2019-02-09 15:22:02 -0500

    Added potentiometer to the Sensors class

--- src/main/java/frc/robot/Sensors.java
+++ src/main/java/frc/robot/Sensors.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 import frc.robot.*;
@@ -12,8 +14,14 @@ public class Sensors {
     public static DigitalInput cargoPresentArm = new DigitalInput(Constants.CARGO_ARM_SENSOR_PORT);
     public static DigitalInput cargoPresentLift = new DigitalInput(Constants.CARGO_LIFT_SENSOR_PORT);
 
+    public static AnalogPotentiometer armPotentiometer = new AnalogPotentiometer(Constants.ARM_POTENTIOMETER_PORT);
+
     public static boolean getDIValue(DigitalInput d) {
         return d.get();
+    }
+
+    public static AnalogPotentiometer getArmPotentiometer() {
+        return armPotentiometer;
     }
 
 }
