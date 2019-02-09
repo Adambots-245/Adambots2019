35ae64331757bdb07ed05b59c82f9702eb7df306
Ethan Rylko <ethan.rylko@gmail.com>
2019-02-09 14:56:03 -0500

    Created Sensors class with four DigitalInputs and a general getter method

---
+++ src/main/java/frc/robot/Sensors.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import frc.robot.*;
+
+public class Sensors {
+
+    public static DigitalInput liftLowered = new DigitalInput(Constants.LIFT_SENSOR_PORT);
+    public static DigitalInput hatchPresent = new DigitalInput(Constants.HATCH_SENSOR_PORT);
+    public static DigitalInput cargoPresentArm = new DigitalInput(Constants.CARGO_ARM_SENSOR_PORT);
+    public static DigitalInput cargoPresentLift = new DigitalInput(Constants.CARGO_LIFT_SENSOR_PORT);
+
+    public static boolean getDIValue(DigitalInput d) {
+        return d.get();
+    }
+
+}
