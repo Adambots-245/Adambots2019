3a6c81db584187a9ddb0dbcba9c8dd063f838d98
245 Drive Station <jack.r.trueax@gmail.com>
2019-02-09 14:25:45 -0500

    Added code to Elevator.java that moves the elevator to the predefined heights. added: findNearestLevel() (which is used by gotoNearestLevel (button actuvate) and passiveNearestLevel(noinput activation))

--- src/main/java/frc/secondary/Elevator.java
+++ src/main/java/frc/secondary/Elevator.java
@@ -1,30 +1,60 @@
 package frc.secondary;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import frc.robot.Actuators;
 import frc.robot.Constants;
 
 public class Elevator {
-        private static boolean Level_1;
-        private static boolean Level_2;
-        private static boolean Level_3;
-        private static int currentPosition;
-        private static int level1Dist;
-        private static int level2Dist;
-        private static int level3Dist;
-        public void init() {
+    private static boolean Level_1;
+    private static boolean Level_2;
+    private static boolean Level_3;
+    private static int level1Dist;
+    private static int level2Dist;
+    private static int level3Dist;
+    private static int nearestDistance;
+    private static int targetPosition;
+
+    public void init() {
         Level_1 = true;
         Level_2 = false;
         Level_3 = false;
-        currentPosition = Constants.LIFT_LEVEL_1;
-        
+
     }
-    public static void findNearestLevel(){
-        currentPosition = Actuators.getLiftMotor1().getSelectedSensorPosition();
+
+    public static int findNearestLevel(int currentPosition) {
         level1Dist = Math.abs(Constants.LIFT_LEVEL_1 - currentPosition);
-        leve21Dist = Math.abs(Constants.LIFT_LEVEL_2 - currentPosition);
-        leve31Dist = Math.abs(Constants.LIFT_LEVEL_3 - currentPosition);
+        level2Dist = Math.abs(Constants.LIFT_LEVEL_2 - currentPosition);
+        level3Dist = Math.abs(Constants.LIFT_LEVEL_3 - currentPosition);
+        nearestDistance = Math.min(Math.min(level1Dist, level2Dist), level3Dist);
+        if (nearestDistance == level1Dist) {
+            targetPosition = Constants.LIFT_LEVEL_1;
+        } else if (nearestDistance == level2Dist) {
+            targetPosition = Constants.LIFT_LEVEL_2;
+        } else {
+            targetPosition = Constants.LIFT_LEVEL_3;
+        }
+        return targetPosition;
     }
+    public static void passiveGotoNearestLevel(double speed){
+        if (speed == 0){
+            int currentPosition = Actuators.getLiftMotor1().getSelectedSensorPosition();
+            int targetPosition = findNearestLevel(currentPosition);
+            Actuators.getLiftMotor1().set(ControlMode.Position, targetPosition);
+        }
+    }
+    public static void gotoNearestLevel(boolean go) {
+        if (go) {
+            int currentPosition = Actuators.getLiftMotor1().getSelectedSensorPosition();
+            int targetPosition = findNearestLevel(currentPosition);
+            Actuators.getLiftMotor1().set(ControlMode.Position, targetPosition);
+        }
+    }
+
+    public static void setLiftMotorSpeed(double speed){
+        Actuators.getLiftMotor1().set(ControlMode.PercentOutput, speed);
+    }
+
     public static void toggleElevator(double elevateSpeed, boolean height) {
         double liftSpeed = Math.min(elevateSpeed, Constants.MAX_LIFT_SPEED);
         Actuators.getLiftMotor1().set(ControlMode.PercentOutput, liftSpeed);
@@ -33,13 +63,11 @@ public class Elevator {
                 Level_2 = true;
                 Level_1 = false;
                 Actuators.getLiftMotor1().set(ControlMode.Position, Constants.LIFT_LEVEL_2);
-            }
-            else if (Level_2) {
+            } else if (Level_2) {
                 Level_3 = true;
                 Level_2 = false;
                 Actuators.getLiftMotor1().set(ControlMode.Position, Constants.LIFT_LEVEL_3);
-            }
-            else {
+            } else {
                 Level_1 = true;
                 Level_3 = false;
                 Actuators.getLiftMotor1().set(ControlMode.Position, Constants.LIFT_LEVEL_1);
