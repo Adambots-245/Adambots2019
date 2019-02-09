85315fbd89d2091d48bbe3f64252465366b966f1
Ethan Rylko <ethan.rylko@gmail.com>
2019-02-09 15:42:46 -0500

    Redid various things, arm now moves at a constant speed, less variables...

--- src/main/java/frc/secondary/Cargo.java
+++ src/main/java/frc/secondary/Cargo.java
@@ -6,22 +6,24 @@ import frc.robot.*;
 
 public class Cargo {
 
-    public static void cargoIntake(double pivotArmSpeed, double inOutSpeed) {
+    // If sensor is blocked, stop the intake and move the arm
+    // If not, set the intake to the input of the triggers
+    public static void cargoIntake(double speed) {
         if (Sensors.getDIValue(Sensors.cargoPresentArm)){
             Actuators.getInfeedArmMotor().set(ControlMode.PercentOutput, Constants.STOP_MOTOR_SPEED);
-            moveArm(pivotArmSpeed);
+            moveArm(speed > 0);
         } else {
-            Actuators.getInfeedArmMotor().set(ControlMode.PercentOutput, inOutSpeed);
+            Actuators.getInfeedArmMotor().set(ControlMode.PercentOutput, speed);
         }
     }
 
-    private static void moveArm(double speed) {
-        boolean positive = (speed > 0);
+    // Moves the arm at a constant rate so long as it is within the thresholds
+    private static void moveArm(boolean positive) {
         double armPos = Sensors.getArmPotentiometer().get();
         if (positive && armPos < Constants.ARM_POTENTIOMETER_MAX) {
-            Actuators.getClimbMotor().set(ControlMode.PercentOutput, speed);
+            Actuators.getClimbMotor().set(ControlMode.PercentOutput, Constants.HALF_MOTOR_SPEED);
         } else if (!positive && armPos > Constants.ARM_POTENTIOMETER_MIN){
-            Actuators.getClimbMotor().set(ControlMode.PercentOutput, speed);
+            Actuators.getClimbMotor().set(ControlMode.PercentOutput, -Constants.HALF_MOTOR_SPEED);
         }
     }
 
