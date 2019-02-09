438f85662adf327e38443af2b8d15142b855fe1e
Ethan Rylko <ethan.rylko@gmail.com>
2019-02-09 13:31:43 -0500

    Rearranged a bit

--- src/main/java/frc/secondary/Elevator.java
+++ src/main/java/frc/secondary/Elevator.java
@@ -12,19 +12,6 @@ public class Elevator {
         setLevel();
     }
 
-    public static int findNearestLevel(){
-        int currentPosition = Actuators.getLiftMotor1().getSelectedSensorPosition();
-        int level1Dist = Math.abs(Constants.LIFT_LEVEL_1 - currentPosition);
-        int level2Dist = Math.abs(Constants.LIFT_LEVEL_2 - currentPosition);
-        int level3Dist = Math.abs(Constants.LIFT_LEVEL_3 - currentPosition);
-        if (level1Dist < level2Dist && level1Dist < level3Dist) {
-            return Constants.LIFT_LEVEL_1;
-        } else if (level2Dist < level1Dist && level2Dist < level3Dist) {
-            return Constants.LIFT_LEVEL_2;
-        }
-        return Constants.LIFT_LEVEL_3;
-    }
-
     public static void move(int speed) {
         Actuators.getLiftMotor1().set(ControlMode.PercentOutput, speed);
         setLevel();
@@ -38,6 +25,19 @@ public class Elevator {
     public static void goToNearest() {
         Actuators.getLiftMotor1().set(ControlMode.Position, findNearestLevel());
         setLevel();
+    }
+
+    public static int findNearestLevel(){
+        int currentPosition = Actuators.getLiftMotor1().getSelectedSensorPosition();
+        int level1Dist = Math.abs(Constants.LIFT_LEVEL_1 - currentPosition);
+        int level2Dist = Math.abs(Constants.LIFT_LEVEL_2 - currentPosition);
+        int level3Dist = Math.abs(Constants.LIFT_LEVEL_3 - currentPosition);
+        if (level1Dist < level2Dist && level1Dist < level3Dist) {
+            return Constants.LIFT_LEVEL_1;
+        } else if (level2Dist < level1Dist && level2Dist < level3Dist) {
+            return Constants.LIFT_LEVEL_2;
+        }
+        return Constants.LIFT_LEVEL_3;
     }
 
     public static void setLevel() {
