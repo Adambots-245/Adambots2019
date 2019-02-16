1a98002cc695b59b6f71846d5ba23bbe5e162e9c
Ethan Rylko <ethan.rylko@gmail.com>
2019-02-15 21:24:47 -0500

    Added many constants for ghostMode recorder

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -70,4 +70,30 @@ public class Constants{
     //Limiters TODO: UPDATE MAX AND MIN CLIMB SPEEDS TO CORRECT VALUES
     public static final double MAX_CLIMB_SPEED = 0.9;
     public static final double MIN_CLIMB_SPEED = -0.9;
+
+    // Recorder constants
+    public static final int MAX_INDEX = 750;
+    public static final int RECORDED_VALUES_AMOUNT = 38;
+    public static final int RECORDER_SLEEP_TIME = 20;
+
+	public static final int TIME_INDEX = 0; 
+    public static final int PRIMARY_LEFT_JOY_X = 1;
+    public static final int PRIMARY_LEFT_JOY_Y = 2;
+    public static final int PRIMARY_RIGHT_JOY_X = 3;
+    public static final int PRIMARY_RIGHT_JOY_Y = 4;
+    public static final int PRIMARY_LEFT_TRIGGER = 5;
+    public static final int PRIMARY_RIGHT_TRIGGER = 6;
+    public static final int PRIMARY_A = 7;
+    public static final int PRIMARY_B = 8;
+    public static final int PRIMARY_X = 9;
+    public static final int PRIMARY_Y = 10;
+    public static final int PRIMARY_START = 11;
+    public static final int PRIMARY_BACK = 12;
+    public static final int PRIMARY_LB = 13;
+    public static final int PRIMARY_RB = 14;
+    public static final int PRIMARY_DPAD_UP = 15;
+    public static final int PRIMARY_DPAD_DOWN = 16;
+    public static final int PRIMARY_DPAD_LEFT = 17;
+    public static final int PRIMARY_DPAD_RIGHT = 18;
+
 }
