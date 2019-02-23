9df20040cc94d9af35d004469f2c4e6215ee6bf6
Ethan Rylko <ethan.rylko@gmail.com>
2019-02-23 10:29:05 -0500

    Added Compressor and info on Dash

--- src/main/java/frc/robot/Actuators.java
+++ src/main/java/frc/robot/Actuators.java
@@ -32,6 +32,8 @@ public class Actuators {
     private static Solenoid CargoHatchDelivery;
     private static DoubleSolenoid ShiftHighGear;
 
+    private static Compressor Compressor1;
+
     public static void init() {
 
         // TODO: add 3rd motor, talk to Mars
@@ -56,6 +58,8 @@ public class Actuators {
         HatchClampOpen = new Solenoid(1, Constants.OPEN_HATCH_CLAMP);
         CargoHatchDelivery = new Solenoid(Constants.ADVANCE_CARGO_HATCH_DELVERY);
         ShiftHighGear = new DoubleSolenoid(Constants.SHIFT_HIGH_SPEED, Constants.SHIFT_LOW_SPEED);
+
+        Compressor1 = new Compressor(Constants.COMPRESSOR_1_PORT);        
 
         // set follower motors
         Left2Motor.follow(Left1Motor);
@@ -171,6 +175,10 @@ public class Actuators {
 
     public static VictorSPX getRight3Motor() {
         return Right3Motor;
+    }
+
+    public static Compressor getCompressor1() {
+        return compressor1;
     }
 
 }
--- src/main/java/frc/robot/Dash.java
+++ src/main/java/frc/robot/Dash.java
@@ -4,34 +4,53 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dash {
+
     public static void init() {
         putMotorOutputs();
         putAutoShiftEnabled();
         putDriveVelocity();
         putSecondaryLeftYValue();
+        putCompressorValues();
+
     }
-    public static void dash(){
+
+    public static void dash() {
         putMotorOutputs();
         putAutoShiftEnabled();
         putDriveVelocity();
         putSecondaryLeftYValue();
+        putCompressorValues();
+
     }
-    public static void putAutoShiftEnabled(){
+
+    public static void putAutoShiftEnabled() {
         SmartDashboard.putBoolean("auto shift enabled", Drive.getAutoShiftState());
     }
-    public static void putMotorOutputs(){
+
+    public static void putMotorOutputs() {
         SmartDashboard.putNumber("left 1", Actuators.getLeft1Motor().getMotorOutputPercent());
         SmartDashboard.putNumber("left 2", Actuators.getLeft2Motor().getMotorOutputPercent());
         SmartDashboard.putNumber("left 3", Actuators.getLeft3Motor().getMotorOutputPercent());
         SmartDashboard.putNumber("right 1", Actuators.getRight1Motor().getMotorOutputPercent());
         SmartDashboard.putNumber("right 2", Actuators.getRight2Motor().getMotorOutputPercent());
         SmartDashboard.putNumber("right 3", Actuators.getRight3Motor().getMotorOutputPercent());
+
     }
-    public static void putDriveVelocity(){
+
+    public static void putDriveVelocity() {
         SmartDashboard.putNumber("left velocity", Actuators.getLeft1Motor().getSelectedSensorVelocity());
         SmartDashboard.putNumber("right velocity", Actuators.getRight1Motor().getSelectedSensorVelocity());
+
     }
-    public static void putSecondaryLeftYValue(){
+
+    public static void putSecondaryLeftYValue() {
         SmartDashboard.putNumber("secondary left y", -Gamepad.secondary.getLeftY().get());
     }
+
+    public static void putCompressorValues() {
+        SmartDashboard.putNumber("compressor 1 current", Actuators.getCompressor1().getCompressorCurrent());
+        SmartDashboard.putBoolean("compressor 1 running", Actuators.getCompressor1().enabled());
+
+    }
+
 }
